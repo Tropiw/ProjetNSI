9aaba6533c60988631cc5921c8fc81d192a63c34
Tropix <tropitags@gmail.com>
2024-02-14 12:01:07 +0100

    main 2h nsi

--- main.py
+++ main.py
@@ -1,2 +1,34 @@
-print('hello world')
-'https://prod.liveshare.vsengsaas.visualstudio.com/join?E829252AC5C79AEAD9731AA8F4F6B87334F3'
+class main:
+    def __init__(self):
+        pass
+    
+    def start():
+        # pygame setup
+        import pygame
+        pygame.init()
+        screen = pygame.display.set_mode((1280, 720))
+        clock = pygame.time.Clock()
+        clock.tick(60)
+        running = True
+
+        while running:
+            # quitter la page quand l'utilisateur clique sur X
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+            # remplir l'ecran de la couleur
+            screen.fill("white")
+
+            # rendre le jeux ici
+            
+
+            # flip() pour mettre le la fenêtre de jeusur l'écran
+            pygame.display.flip()
+
+        pygame.quit()
+    
+main.start()
+    
+    
+    
