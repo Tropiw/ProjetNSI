018789707210df68745a67f27486a30e1748d2aa
Tropix <tropitags@gmail.com>
2024-02-20 11:56:41 +0100

    Update 10-12h mardi jules absent

--- Imagesetter.py
+++ Imagesetter.py
@@ -5,6 +5,7 @@ class tile:
     SIZE = 16 # width and length
 
     def __init__(self, fichier, x = None, y = None):
+        self.cache_tile = {} #cache des tiles 
         self.img_set = Image.open(fichier)
         if x != None and y!= None:
             self.load(x, y)
@@ -13,13 +14,13 @@ class tile:
 
 
     def debug_afficher(self):
-        self.img.show()
+        print('cache:',self.cache_tile )
 
     def load(self,i, j):
         pil_image = self.img.crop(
             (
                 i * tile.SIZE, #gauche
-                j*tile.SIZE, #haut
+                j * tile.SIZE, #haut
                 (i+1)*tile.SIZE, #droite
                 (j+1)*tile.SIZE #bas
             ))
@@ -29,7 +30,7 @@ class tile:
         pygame_surface = pygame.image.fromstring(image_data, image_dimensions, mode)
         pygame_surface = pygame.transform.scale(pygame_surface, (80, 80))
         self.img_tile = pygame_surface
-        return pygame_surface
+        self.cache_tile['('+str(i)+','+str(j)+')'] = pygame_surface ##insertion des tile dans le cache 
     
     
     def render(self, screen,tile_pos,coordonate):
@@ -37,10 +38,12 @@ class tile:
             screen.blit((self.img_tile),(coordonate))
             
             
-    def blit_tile(screen,tile_pos,coordonate):
+    '''def blit_tile(screen,tile_pos,coordonate):
         # utilisation d'un cache
-        # dictionnaire qui prend 'image en clé et les coordonnées 
-        screen.blit(tile('Graphic\Dungeon Gathering Free Version\Set 1.1.png').load(tile_pos[0],tile_pos[1]),(coordonate))
-
-
-
+        # dictionnaire qui prend 'image en clé et les coordonnées
+        dictionnaire_cache_map = dict()
+        for i in range(16):
+            for j in range(16):
+                dictionnaire_cache_map.append(screen.blit(tile('Graphic\Dungeon Gathering Free Version\Set 1.1.png').load(tile_pos[i],tile_pos[j]))),(i,j)):
+            
+                #tu fais quoi??'''
---
+++ sandbox_imagesetter.py
@@ -0,0 +1,7 @@
+import pygame
+import Imagesetter as image
+
+
+pomme = image.tile('Graphic/Dungeon Gathering Free Version/Set 1.1.png')
+
+pomme.debug_afficher()
